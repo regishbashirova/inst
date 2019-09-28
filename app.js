5d552285d7f693435bfaf967d6fb17740070c617
“regishbashirova“ <regishbashirova@gmail.com>
2019-09-28 10:57:16 +0300

    переименование переменных

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('worked')
 
-const author = {
+const buzova = {
     id: 'buzova86',
     avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com'
     verified: true,
@@ -8,7 +8,7 @@ const author = {
 
 const photo = {
     id: 'B23d82NooNF';
-    author: author,
+    author: buzova,
     location: 'Набережная Амура',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: null,
@@ -17,7 +17,7 @@ const photo = {
 
 const video = {
     id: 'B23000000';
-    author: author,
+    author: buzova,
     location: 'ТЦ Облака',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: 'https://youtube.com/...',
