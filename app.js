c450e45117553ad58afb4ac1eebc0376a587bcd2
“regishbashirova“ <regishbashirova@gmail.com>
2019-09-28 10:42:27 +0300

    photo&video posts

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('worked')
+
+const author = {
+    id: 'buzova86',
+    avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com'
+    verified: true,
+}
+
+const photo = {
+    id: 'B23d82NooNF';
+    author: author,
+    location: 'Набережная Амура',
+    imageUrl: 'https://placekitten.com/800/800',
+    videoUrl: null,
+    likes: 150000,
+};
+
+const video = {
+    id: 'B23000000';
+    author: author,
+    location: 'ТЦ Облака',
+    imageUrl: 'https://placekitten.com/800/800',
+    videoUrl: 'https://youtube.com/...',
+    likes: 1500,
+
+}
